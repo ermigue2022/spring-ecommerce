c2bbb6c7e123a43a8dea7bf992e28e1a4f5b9a70
Ermigue2022 <ermigue2022@gmail.com>
2022-08-21 18:56:47 +0200

    crear el repository y las clases y el metodo show de ProductoController

---
+++ src/main/java/com/curso/ecommerce/service/ProductoService.java
@@ -0,0 +1,18 @@
+package com.curso.ecommerce.service;
+
+import java.util.Optional;
+
+import com.curso.ecommerce.model.Producto;
+
+public interface ProductoService {//aqui definiremos nuestro CRUD
+	
+	public Producto save(Producto producto);
+	public Optional<Producto> get(Integer id); //retorna un Optional de tipo producto,pq nos da la opción de validad si 
+											   // el objeto existe o no
+	public void update(Producto producto);
+	public void delete(Integer id);
+	
+	
+	
+
+}
