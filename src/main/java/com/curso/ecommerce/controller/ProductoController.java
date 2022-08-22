1c65f94269e7c2256f8c1ed16254421c36a203ac
Ermigue2022 <ermigue2022@gmail.com>
2022-08-22 17:17:49 +0200

    funcionalidad mostrar productos para el administrador

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -4,6 +4,7 @@ package com.curso.ecommerce.controller;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,8 @@ public class ProductoController {
 	private ProductoService productoService;
 	
 	@GetMapping("")
-	public String show() {
+	public String show(Model model) { //Objeto Model, lleva información desde el backend hacia la vista
+		model.addAttribute("productos", productoService.findAll());
 		return "productos/show";
 	}
 	@GetMapping("/create")
--- src/main/java/com/curso/ecommerce/service/ProductoService.java
+++ src/main/java/com/curso/ecommerce/service/ProductoService.java
@@ -1,5 +1,6 @@
 package com.curso.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.curso.ecommerce.model.Producto;
@@ -11,6 +12,7 @@ public interface ProductoService {//aqui definiremos nuestro CRUD
 											   // el objeto existe o no
 	public void update(Producto producto);
 	public void delete(Integer id);
+	public List<Producto> findAll();
 	
 	
 	
