5f1c7395be82bf084ec42e732ad3b21c8e90404a
Ermigue2022 <ermigue2022@gmail.com>
2022-08-24 21:26:07 +0200

    redireccion al carrito de compras

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.model.Producto;
@@ -17,26 +18,32 @@ import com.curso.ecommerce.service.ProductoService;
 @Controller
 @RequestMapping("/")
 public class HomeController {
-	
+
 	private final Logger log = LoggerFactory.getLogger(HomeController.class);
-	
+
 	@Autowired
 	private ProductoService productoService;
-	
+
 	@GetMapping("")
 	public String home(Model model) {
-		
+
 		model.addAttribute("productos", productoService.findAll());
 		return "usuario/home";
 	}
+
 	@GetMapping("productohome/{id}")
 	public String productoHome(@PathVariable Integer id, Model model) {
 		log.info("Id producto enviado como parámetro {}", id);
 		Producto producto = new Producto();
 		Optional<Producto> productoOptional = productoService.get(id);
 		producto = productoOptional.get();
-		model.addAttribute("producto",producto);
+		model.addAttribute("producto", producto);
 		return "usuario/productohome";
+	}
+
+	@PostMapping("/cart")
+	public String addCart() {
+		return "usuario/carrito";
 	}
 
 }
