0628e63045500a10d3905ef8bc9dde9c7741597e
Ermigue2022 <ermigue2022@gmail.com>
2022-09-01 21:23:37 +0200

    version ya en la nube, con el arreglo de las imagenes

--- src/main/java/com/curso/ecommerce/ResourceWebConfiguration.java
+++ src/main/java/com/curso/ecommerce/ResourceWebConfiguration.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class ResourceWebConfiguration implements WebMvcConfigurer{
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/images/**").addResourceLocations("file:images/");
+		registry.addResourceHandler("/images/**").addResourceLocations("file:/opt/images/");
 	}
 }
